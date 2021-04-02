f574b61fbb57d9868e3104971615c550b6df6962
vupa2 <anhvu.pa96@gmail.com>
2021-04-02 17:13:20 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,18 @@
 <?php
-  if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $data = [
-      'invesment' => $_POST['investment'],
-      'interest' => $_POST['interest'],
-      'year' => $_POST['year']
-    ];
+
+function calculateFutureValue($investment, $interest, $year) {
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  $errors = [];
+  
+  foreach ($_POST as $key => $value) {
+    if (!is_numeric($value)) {
+      $errors[] = "Please fill $key";
+    }
   }
+  
+}
 ?>
 
 <!doctype html>
@@ -26,22 +33,28 @@
   <h1>[Bài tập] Ứng dụng Future Value Calculator</h1>
   <form action="" method="post">
     <div>
-      <input type="number" name="investment" id="investment" value="<?= $data['invesment']; ?>">
+      <input type="number" name="investment" id="investment" value="<?= $data['invesment'] ?>">
       <label for="investment">Investment Amount</label>
     </div>
     <div>
-      <input type="number" name="interest" id="interest" value="<?= $data['interest']; ?>">
+      <input type="number" name="interest" id="interest" value="<?= $data['interest'] ?>">
       <label for="interest">Yearly Interest Rate</label>
     </div>
     <div>
-      <input type="number" name="year" id="year" value="<?= $data['year']; ?>">
+      <input type="number" name="year" id="year" value="<?= $data['year'] ?>">
       <label for="year">Number of Years</label>
     </div>
     <div>
-    <button type="submit">Submit</button>
+      <button type="submit">Submit</button>
     </div>
   </form>
   
-  <h4><?= $result; ?></h4>
+  <?php if (!empty($errors)) : ?>
+    <?php foreach($errors as $value) : ?>
+      <h4><?= $value ?></h4>
+    <?php endforeach; ?>
+  <?php endif; ?>
+  
+  <h4><?= $result ?></h4>
 </body>
 </html>
